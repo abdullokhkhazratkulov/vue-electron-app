1cee80e5922abe9f7602f6ac49ecf2c8ae8bed84
Abdullokh <abdullokhkhazratkulov@gmail.com>
2022-12-04 23:44:43 +0500

    test-3

--- src/main.js
+++ src/main.js
@@ -9,9 +9,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
-// Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 new Vue({
   router,
